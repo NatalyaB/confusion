3477cc9f202a2320f5c89890d609949fc475022c
Natalya <nata.basovska@gmail.com>
2019-07-16 11:11:53 -0400

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -24,8 +24,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
     }
         
 
-    function RenderComments({comments}) {
-
+    function RenderComments({comments, addComment, dishId})  {
 
         const comment = comments.map((commentItem) => {
             return (
@@ -45,7 +44,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
                     <div>
                         {comment}
                     </div>
-                    <CommentForm></CommentForm>
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </div>
             );
 
@@ -69,8 +68,8 @@ class CommentForm extends Component  {
     }
 
     handleSubmit(values) {
-        console.log("Current State is: " +JSON.stringify(values));
-        alert("Current State is: " +JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     
@@ -102,16 +101,16 @@ class CommentForm extends Component  {
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Label htmlFor="yourname" md={3}>Your Name</Label>
+                                <Label htmlFor="author" md={3}>Your Name</Label>
                                 <Col md={12}>
-                                    <Control.text model=".yourname" id="yourname" name="yourname" 
+                                    <Control.text model=".author" id="author" name="author" 
                                         placeholder="Your Name"  
                                         className="form-control"
                                         validators={{
                                             minLength: minLength(3), maxLength: maxLength(15)}}
                                         />
                                     <Errors 
-                                        className="text-danger" model=".yourname"
+                                        className="text-danger" model=".author"
                                         show="touched" 
                                         messages={{
                                             minLength: 'Must be greater than 2 characters',
@@ -121,9 +120,9 @@ class CommentForm extends Component  {
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Label htmlFor="message" md={2}>Comment</Label>
+                                <Label htmlFor="comment" md={2}>Comment</Label>
                                 <Col md={12}>
-                                    <Control.textarea model=".message" id="message" name="message" 
+                                    <Control.textarea model=".comment" id="comment" name="comment" 
                                            rows="6" className="form-control" />
                                 </Col>
                             </Row>
@@ -162,7 +161,10 @@ const Dishdetail = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id}
+                    />
                 </div>
             </div>
         );
